df1b97b1a86788a61d1b28b93a56b9b4b7620296
Ved Sampath <vedsampath@gmail.com>
2024-09-15 17:31:16 -0700

    fix port

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,6 @@ app.use(cors());
 
 // Start the server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
